1172eb857719f775cbc8c162f32ff9f8228617bf
Anhzf <36941633+Epiclygend@users.noreply.github.com>
2021-01-21 12:59:32 +0700

    feat: :sparkles: setup twin.macro & code styles

---
+++ components/elements/Button.tsx
@@ -0,0 +1,5 @@
+import tw, { styled } from 'twin.macro'
+
+export default styled.button(() => [
+  tw`text-lg border-2 p-4`,
+]);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/dist/next-server/lib/router/router'
+import GlobalStyles from '../components/GlobalStyles'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
