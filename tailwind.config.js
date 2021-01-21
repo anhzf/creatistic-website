f86d0d966219497548ba2cd0638b457fc85293e6
Anhzf <36941633+Epiclygend@users.noreply.github.com>
2021-01-21 15:27:28 +0700

    build: :wrench: fix purge paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   purge: [
     './pages/**/*.js',
-    './components/**/*.js'
+    './components/**/*.js',
+    './pages/**/*.jsx',
+    './components/**/*.jsx',
+    './pages/**/*.ts',
+    './components/**/*.ts',
+    './pages/**/*.tsx',
+    './components/**/*.tsx',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
