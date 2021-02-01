6f7b33feb669db58f5d92467b09fb2417bc61780
Anhzf <36941633+Epiclygend@users.noreply.github.com>
2021-02-01 17:26:10 +0700

    feat: :lipstick: edit email template

--- components/mails/Greeting.tsx
+++ components/mails/Greeting.tsx
@@ -4,11 +4,39 @@ interface Props {
 
 export default function GreetingMail({ name }: Props) {
   return (
-    <main>
-      <h1>Terimakasih {name} 👋 telah berlangganan info-info menarik dari kami!</h1>
-      <h2>Tunggu informasi menarik dari kami selanjutnya ya...!</h2>
-      <hr/>
-      <span>Salam hangat dari <a href="https://creatistic.id">Creatistic.ID</a></span>
-    </main>
+    <article>
+      <img src="/assets/mail-header.jpeg" alt="header email creatisticID"/>
+      <br/>
+      <font size="6">Terimakasih {name} 👋</font>
+
+      <div>
+        <font size="4">Terimakasih {name}... telah berlangganan dan bergabung menjadi bagian dari creatistic.id.</font>
+      </div>
+
+      <div>
+        <font size="4">
+          <br/>
+        </font>
+      </div>
+
+      <div>
+        Komitmen kami memberikan yang terbaik dalam setiap pesanan anda. Karena motto kami adalah mewujudkan cinta dalam furniture.
+        <br/>
+        Berlangganan dan bergabung bersama creatistic.id turut menjadi perwujudan cinta produk Indonesia dan pengembangan masyarakat sekitar.
+        <br/>
+        <br/>
+        <blockquote>Creatistic.id bermanfaat bagi masyarakat.</blockquote>
+        <br/>
+        Tunggu informasi menarik dari kami selanjutnya ya...!
+        <hr/>
+        Salam hangat dari <a href="https://creatistic.id/">creatistic.id</a>
+        <br/>
+        <small>
+          Merasa tidak berlangganan? silahkan hubungi kami melalui email kami ya...
+          <br/>
+          <a href="mailto:creatistic.id@gmail.com">creatistic.id@gmail.com</a>
+        </small>
+      </div>
+    </article>
   )
 }
