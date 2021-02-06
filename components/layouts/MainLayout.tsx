aee1a0093897692535e130bf1f66cc1c36fcbd83
Anhzf <36941633+Epiclygend@users.noreply.github.com>
2021-02-06 19:30:46 +0700

    feat: :tada: cleanup coming soon prepare for awesome

---
+++ components/layouts/MainLayout.tsx
@@ -0,0 +1,48 @@
+import Head from 'next/head';
+import { useRouter } from 'next/router'
+import tw from 'twin.macro';
+
+const Container = tw.div`min-h-screen w-screen flex flex-col items-center`;
+
+interface Props extends React.HTMLAttributes<HTMLDivElement> {
+  title?: string;
+  desc?: string;
+  metaUrl?: string;
+}
+
+export default function MainLayout({
+  title = 'Creatistic Official Website - Mengabadikan Cinta Dalam Furnitur',
+  desc = 'Menjawab kebutuhan furnitur anda, menciptakan cinta dalam furnitur. Memberikan anda suatu hal yang baru intuk kemajuan negri Indonesia melalui pengembangan UKM. Creatistic.id untuk Indonesia.',
+  metaUrl = useRouter().pathname,
+  children,
+  ...props
+}: Props) {
+  return (
+    <Container {...props}>
+      <Head>
+        {/* Primary Meta Tags */}
+        <title>{title}</title>
+        <meta name="title" content={title} />
+        <meta name="description" content={desc} />
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={desc} />
+        <meta property="og:url" content={metaUrl} />
+        <meta property="og:image" content="/assets/logo creatistic.png" />
+        <link rel="icon" href="/favicon.ico" />
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content={metaUrl} />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={desc} />
+        <meta property="twitter:image" content={metaUrl} />
+        {/* Indexing */}
+        <meta name="robots" content="index, follow" />
+        <meta name="google-site-verification" content="yRVHmDY9GNvr828pyT4AW_pb4NiRamy3mTdbVEucY2I" />
+      </Head>
+
+      {children}
+    </Container>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,15 @@
 import { ReactNode, useEffect, useState } from 'react';
-import Head from 'next/head';
-import Image from 'next/image';
-import { useRouter } from 'next/router';
 import tw from 'twin.macro';
-import { HiX, HiMail } from 'react-icons/hi'
+import { HiX, HiMail } from 'react-icons/hi';
 import SubscribeNewsletter from 'components/SubscribeNewsletter';
 import Overlay from 'components/elements/Overlay';
 import Alert from 'components/elements/Alert';
+import MainLayout from 'components/layouts/MainLayout';
 
-const title = 'Creatistic Official Website - Mengabadikan Cinta Dalam Furnitur';
-const desc = 'Menjawab kebutuhan furnitur anda, menciptakan cinta dalam furnitur. Memberikan anda suatu hal yang baru intuk kemajuan negri Indonesia melalui pengembangan UKM. Creatistic.id untuk Indonesia.';
-const Container = tw.div`min-h-screen w-screen flex flex-col items-center justify-center`;
 const MainContainer = tw.main`flex flex-col items-center justify-center`;
-const HeaderContentContainer = tw.section`absolute left-1/2 bottom-0 w-full max-w-screen-md px-5 py-10 transform -translate-x-1/2 lg:-translate-y-10 2xl:-translate-y-20 flex flex-col justify-center text-white`;
+const ALERT_TIMEOUT = 10000;
 
 export default function Home() {
-  const ALERT_TIMEOUT = 10000;
-  const router = useRouter();
   const [isSubscribeModalOpen, setIsSubscribeModalOpen] = useState(false);
   const [alertMsg, setAlertMsg] = useState<ReactNode | null>(null);
 
@@ -31,98 +24,44 @@ export default function Home() {
   }, [alertMsg])
 
   return (
-    <Container>
-      <Head>
-        {/* Primary Meta Tags */}
-        <title>{title}</title>
-        <meta name="title" content={title} />
-        <meta name="description" content={desc} />
-        {/* Open Graph / Facebook */}
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={desc} />
-        <meta property="og:url" content={router.pathname} />
-        <meta property="og:image" content="/assets/logo creatistic.png" />
-        <link rel="icon" href="/favicon.ico" />
-        {/* Twitter */}
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content={router.pathname} />
-        <meta property="twitter:title" content={title} />
-        <meta property="twitter:description" content={desc} />
-        <meta property="twitter:image" content={router.pathname} />
-        {/* Indexing */}
-        <meta name="robots" content="index, follow" />
-        <meta name="google-site-verification" content="yRVHmDY9GNvr828pyT4AW_pb4NiRamy3mTdbVEucY2I" />
-      </Head>
-
-      <header className="relative h-screen w-full font-semibold">
-        <Image
-          src="/assets/bg panjang.jpg"
-          layout="fill"
-          objectFit="cover"
-          alt="logo creatistic"
-        />
-
-        <div className="absolute bottom-1/4 right-0">
-          <Image
-            src="/assets/bg.png"
-            width={800}
-            height={450}
-            layout="intrinsic"
-            objectFit="contain"
-          />
-        </div>
-
-        <div className="absolute left-1/2 sm:left-1/3 lg:left-1/4 md:right-1/3 top-8 sm:top-2 transform -translate-x-1/2">
-          <Image
-            src="/assets/logo creatistic.png"
-            width={300}
-            height={300}
-            layout="intrinsic"
-            objectFit="contain"
-          />
-        </div>
-
-        <HeaderContentContainer>
-          <h2 className="my-2 font-mont text-6xl sm:text-8xl xl:text-9xl">Coming Soon</h2>
-
-          <h3 className="font-light sm:text-lg">Menjawab kebutuhan furnitur anda, menciptakan cinta dalam furnitur. Memberikan anda suatu hal yang baru intuk kemajuan negri Indonesia melalui pengembangan UKM. Creatistic.id untuk Indonesia.</h3>
-        </HeaderContentContainer>
+    <MainLayout>
+      <header className="relative h-screen w-full">
       </header>
 
       <MainContainer>
-        {isSubscribeModalOpen ?
-          <Overlay className="flex flex-col justify-center">
-            <Overlay.CloseButton onClick={() => setIsSubscribeModalOpen(false)}>
-              <HiX className="mx-auto text-2xl" />
-            </Overlay.CloseButton>
-
-            <SubscribeNewsletter
-              title="Dapatkan info menarik dari kita!"
-              description="Daftarkan emailmu untuk menjadi orang pertama yang mendapat info menarik dan juga info giveaway dari kita!"
-              onNewSubscriberRegistered={(data) => {
-                setIsSubscribeModalOpen(false);
-                setAlertMsg((
-                  <h1 className="font-medium text-3xl text-gray-900">Terima kasih{' '}
-                    <span className="font-bold text-blue-500">{data.name}</span>! Ada sesuatu nunggu di email kamu nih...✨
-                  </h1>
-                ));
-              }}
-            />
-          </Overlay>
-          : (
-            <SubscribeNewsletter.Fab onClick={() => setIsSubscribeModalOpen(true)}>
-              <HiMail className="m-auto text-3xl text-gray-900"/>
-            </SubscribeNewsletter.Fab>
-          )
-        }
-
-        {alertMsg &&
-          <Alert timeout={ALERT_TIMEOUT}>
-            {alertMsg}
-          </Alert>
-        }
       </MainContainer>
-    </Container>
+
+      {isSubscribeModalOpen ?
+        <Overlay className="flex flex-col justify-center">
+          <Overlay.CloseButton onClick={() => setIsSubscribeModalOpen(false)}>
+            <HiX className="mx-auto text-2xl" />
+          </Overlay.CloseButton>
+
+          <SubscribeNewsletter
+            title="Dapatkan info menarik dari kita!"
+            description="Daftarkan emailmu untuk menjadi orang pertama yang mendapat info menarik dan juga info giveaway dari kita!"
+            onNewSubscriberRegistered={(data) => {
+              setIsSubscribeModalOpen(false);
+              setAlertMsg((
+                <h1 className="font-medium text-3xl text-gray-900">Terima kasih{' '}
+                  <span className="font-bold text-blue-500">{data.name}</span>! Ada sesuatu nunggu di email kamu nih...✨
+                </h1>
+              ));
+            }}
+          />
+        </Overlay>
+        : (
+          <SubscribeNewsletter.Fab onClick={() => setIsSubscribeModalOpen(true)}>
+            <HiMail className="m-auto text-3xl text-gray-900" />
+          </SubscribeNewsletter.Fab>
+        )
+      }
+
+      {alertMsg &&
+        <Alert timeout={ALERT_TIMEOUT}>
+          {alertMsg}
+        </Alert>
+      }
+    </MainLayout>
   )
 }
