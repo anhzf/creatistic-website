fcaf4a80b341753c1b6487eb354a3e69c5b4ce5f
Anhzf <36941633+Epiclygend@users.noreply.github.com>
2021-02-19 21:10:22 +0700

    feat(ui): :sparkles: support handler props for TextInput

--- components/elements/TextInput.tsx
+++ components/elements/TextInput.tsx
@@ -1,5 +1,20 @@
-import tw from 'twin.macro';
+import { Dispatch, SetStateAction } from 'react';
+import 'twin.macro';
 
-const TextInput = tw.input`py-3 px-5 border-2 rounded-full placeholder-gray-500 focus:outline-none focus:ring-4 focus:ring-blue-500 focus:ring-opacity-70`;
+type acceptedValue = JSX.IntrinsicElements['input']['value'];
+
+type Props = JSX.IntrinsicElements['input'] & {
+  handler?: [acceptedValue, Dispatch<SetStateAction<acceptedValue>>];
+}
+
+const TextInput = ({ value, onChange, handler, ...props }: Props) => (
+  // Prior to (value & onChange) than handler props
+  <input
+    value={value || (handler ? handler[0] : undefined)}
+    onChange={onChange || handler ? (e => handler?.[1](e.target.value)) : undefined}
+    tw="py-3 px-5 border-2 rounded-full placeholder-gray-500 focus:outline-none focus:ring-4 focus:ring-blue-500 focus:ring-opacity-70"
+    {...props}
+  />
+);
 
 export default TextInput;
