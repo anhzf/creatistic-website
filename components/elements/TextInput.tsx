cd9a1378e775ee925ef8f3809ba83d0ddb8b4ed8
Anhzf <36941633+Epiclygend@users.noreply.github.com>
2021-02-19 22:00:27 +0700

    fix(ui): :boom: deprecate latest TextInput features

--- components/elements/TextInput.tsx
+++ components/elements/TextInput.tsx
@@ -1,20 +1,5 @@
-import { Dispatch, SetStateAction } from 'react';
-import 'twin.macro';
+import tw from 'twin.macro';
 
-type acceptedValue = JSX.IntrinsicElements['input']['value'];
-
-type Props = JSX.IntrinsicElements['input'] & {
-  handler?: [acceptedValue, Dispatch<SetStateAction<acceptedValue>>];
-}
-
-const TextInput = ({ value, onChange, handler, ...props }: Props) => (
-  // Prior to (value & onChange) than handler props
-  <input
-    value={value || (handler ? handler[0] : undefined)}
-    onChange={onChange || handler ? (e => handler?.[1](e.target.value)) : undefined}
-    tw="py-3 px-5 border-2 rounded-full placeholder-gray-500 focus:outline-none focus:ring-4 focus:ring-blue-500 focus:ring-opacity-70"
-    {...props}
-  />
-);
+const TextInput = tw.input`py-3 px-5 border-2 rounded-full placeholder-gray-500 focus:outline-none focus:ring-4 focus:ring-blue-500 focus:ring-opacity-70`;
 
 export default TextInput;
