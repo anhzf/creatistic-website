da0621f124d6346ec387b708f4ae10dd6c1124ed
Anhzf <36941633+Epiclygend@users.noreply.github.com>
2021-02-26 17:20:05 +0700

    feat(api): :sparkles: structuring orderViaWhatsapp message

--- app/apis/orderViaWhatsapp.ts
+++ app/apis/orderViaWhatsapp.ts
@@ -1,18 +1,52 @@
 import { Order } from 'types/models';
 
+const API_URL = 'http://wa.me/';
 const CONTACT_TARGET = '6282136961875';
 
+const newMessage = function () {
+  let msg = '';
+
+  return {
+    addLine: function (text: string) {
+      msg += `\n${text}`;
+    },
+
+    addRow: function (rowName: string, rowContent: any) {
+      this.addLine(`${rowName}: ${rowContent || '-'}`);
+    },
+
+    toString() {
+      return msg;
+    }
+  };
+};
+
+const shippingAddressToString = ({
+  streetName,
+  village,
+  district,
+  city,
+  province,
+  country
+}: Order['shippingAddress']) => {
+  return `${streetName}, ${village}, ${district}, ${city}, ${province}, ${country}`;
+}
+
 export default function orderViaWhatsapp(order: Order) {
-  const waApiUrl = new URL('http://wa.me/');
+  const waApiUrl = new URL(API_URL);
+  const text = newMessage();
+
+  text.addLine('Salam Kreatif,');
+  text.addRow('Halo min, saya pesan produk', `${order._ui.productName}#${order.productId}`);
+  text.addRow('Varian', '-');
+  text.addRow('Jumlah', order.amount);
+  text.addRow('Nama Pemesan', order.ordererName);
+  text.addRow('Alamat', shippingAddressToString(order.shippingAddress));
+  text.addRow('Nomor HP', order.contactPerson);
+  text.addRow('Keterangan', order.notes);
+
   waApiUrl.pathname = CONTACT_TARGET;
-  waApiUrl.searchParams.append('text', `Salam Kreatif
-  Halo min, saya pesan produk: ${order._ui.productName}#${order.productId}
-  Varian: -
-  Jumlah: ${order.amount}
-  Nama Pemesan: ${order.ordererName}
-  Alamat: ${order.shippingAddress}
-  Nomor Hp: ${order.contactPerson || '-'}
-  Keterangan: ${order.notes || '-'}`);
+  waApiUrl.searchParams.append('text', text.toString());
 
   window.open(waApiUrl.toString());
 };
