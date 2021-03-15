ab8a8bf33fe723228351c621ff92d298a570dc5e
Anhzf <36941633+Epiclygend@users.noreply.github.com>
2021-03-16 02:56:52 +0700

    feat(ui): :construction: improve logic internal structure for Uploader.tsx
    - remove hadler props
    - change previewList to memoize data instead of memoize component
    - file type check before displaying to preview

--- components/Uploader.tsx
+++ components/Uploader.tsx
@@ -1,68 +1,84 @@
-import React, { Dispatch, SetStateAction, useImperativeHandle, useMemo, useState } from 'react';
+import React, { useMemo, useState } from 'react';
+import tw from 'twin.macro';
+import { HiOutlinePlus } from 'react-icons/hi';
 import { FILE_TYPES } from 'app/utils/file';
-import type fb from 'firebase'
+import useFireStorageFileList from 'hooks/useFireStorageFileList';
+import type fb from 'firebase';
+import Img from 'components/elements/Img';
+
+interface IPreviewItem {
+  url: string;
+  fileName: string;
+  fileType: string;
+  isUploaded: boolean;
+}
+
+const PreviewItem = tw.div`relative w-full h-40 bg-white shadow flex flex-col justify-center`;
 
 type Props = {
   storageRef: fb.storage.Reference;
-  handler?: [
-    File[],
-    Dispatch<SetStateAction<File[]>>,
-  ];
   accept?: JSX.IntrinsicElements['input']['accept'];
 };
 
-export interface UploaderRef {
-  upload: () => Promise<fb.storage.UploadTaskSnapshot[]>;
-}
-
-const Uploader = React.forwardRef<UploaderRef, Props>(({
+const Uploader = function ({
   storageRef,
-  handler: [val, setVal] = useState<File[]>([]),
   accept
-}, ref) => {
-  const previewList = useMemo(
-    () => val.map(e => {
-      const objUrl = URL.createObjectURL(e);
-
-      return (
-        <div key={e.name} className="w-full h-40 bg-white shadow flex flex-col justify-center">
-          {FILE_TYPES.image.includes(e.type)
-            && <img src={objUrl} className="h-32 object-cover" />}
-          <a
-            title={e.name}
-            className="w-full text-center truncate hover:text-blue-500 hover:underline"
-            href={objUrl}
-            target="_blank"
-          >
-            {e.name}
-          </a>
-        </div>
-      )
-    }),
-    [val],
-  );
-  const upload = function () {
-    return Promise.all(val.map(i => storageRef.child(i.name).put(i)));
-  };
-  useImperativeHandle(ref, () => ({ upload }), [storageRef]);
+}: Props) {
+  const fileInputId = Date.now().toString();
+  const [files, setFiles] = useState<File[]>([]);
+  const [storageFiles, storageFilesLoading] = useFireStorageFileList(storageRef);
+  const previewItems = useMemo<IPreviewItem[]>(() => [
+    ...storageFiles.map(file => ({
+      url: file.url,
+      fileName: file.metadata.name,
+      fileType: file.metadata.contentType,
+      isUploaded: true,
+    })),
+    ...files.map(item => ({
+      url: URL.createObjectURL(item),
+      fileName: item.name,
+      fileType: item.type,
+      isUploaded: false,
+    })),
+  ], [storageFiles, files]);
 
   return (
     <div className="flex flex-col shadow-md">
-      <div className="bg-gray-100 p-4 grid grid-cols-5 gap-4">
-        {previewList}
+      <div className={`p-4 grid grid-cols-5 gap-4 ${storageFilesLoading ? 'bg-blue-gray-300 animate-pulse' : 'bg-gray-100'}`}>
+        {previewItems.map(el => (
+          <PreviewItem key={el.fileName}>
+            {FILE_TYPES.image.includes(el.fileType)
+              && <Img src={el.url} className="h-32 object-cover" />}
+            <a
+              title={el.fileName}
+              className="w-full text-center truncate hover:text-blue-500 hover:underline"
+              href={el.url}
+              target="_blank"
+            >
+              {el.fileName}
+            </a>
+          </PreviewItem>
+        ))}
+
+        {storageFilesLoading ?
+          <span>Loading files...</span>
+          : (
+            <label htmlFor={fileInputId} role="button" className="cursor-pointer relative w-full h-40 bg-white shadow flex flex-col justify-center items-center">
+              <HiOutlinePlus className="text-5xl text-gray-300" />
+              <input
+                id={fileInputId}
+                type="file"
+                accept={accept}
+                onChange={e => setFiles(Array.from(e.target.files ?? []))}
+                multiple
+                className="absolute top-0 left-0 opacity-0"
+              />
+            </label>
+          )
+        }
       </div>
-
-      <input
-        type="file"
-        accept={accept}
-        onChange={e => setVal(Array.from(e.target.files ?? []))}
-        multiple
-        className="appearance-none"
-      />
-
-      <button onClick={upload}>Upload</button>
     </div>
   );
-});
+};
 
 export default React.memo(Uploader);
